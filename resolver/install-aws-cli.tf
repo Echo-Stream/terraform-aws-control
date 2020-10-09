b97b980c60b87512b311ff2ad9c6d8f9fdaf50be
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-09 22:10:17 +0530

    fix install cli

--- resolver/install-aws-cli.tf
+++ resolver/install-aws-cli.tf
@@ -1,9 +1,7 @@
 resource "null_resource" "install_aws_cli" {
   provisioner "local-exec" {
     command = <<EOH
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip" \
-&& unzip ${path.module}/awscli-bundle.zip -d ${path.module} \
-&& sudo ${path.module}/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip"; unzip ${path.module}/awscli-bundle.zip -d ${path.module}; ${path.module}/awscli-bundle/install -b ~/bin/aws;
 EOH
   }
 }
