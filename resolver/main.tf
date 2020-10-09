9620891303e26c3515be00dac33a2c56d951be9e
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-09 22:37:23 +0530

    test

--- resolver/main.tf
+++ resolver/main.tf
@@ -2,6 +2,6 @@ resource "null_resource" "create_resolver" {
   depends_on = [null_resource.install_aws_cli]
 
   provisioner "local-exec" {
-    command = "aws appsync create-resolver --api-id ${var.api_id} --type-name ${var.type} --field-name ${var.field} --data-source-name ${var.datasource_name}"
+    command = "${path.module}/awscli/bin/aws appsync create-resolver --api-id ${var.api_id} --type-name ${var.type} --field-name ${var.field} --data-source-name ${var.datasource_name}"
   }
 }
