a6fac9b3cb8c3882c00fc4a7f7931dadf6dabd22
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-09 22:07:16 +0530

    test resolver

--- appsync.tf
+++ appsync.tf
@@ -50,6 +50,14 @@ resource "aws_iam_role_policy_attachment" "hl7_ninja_appsync" {
 ## Resolvers ##
 ###############
 
+module "get_message_type" {
+  api_id          = aws_appsync_graphql_api.hl7_ninja.id
+  field           = "GetMessageType"
+  type            = "Query"
+  datasource_name = module.message_type_datasource.datasource_name
+  source          = "./resolver"
+}
+
 #Queries
 
 # resource "aws_appsync_resolver" "GetUsersForTenant" {
@@ -689,6 +697,16 @@ module "app_datasource" {
   invoke_lambda_policy_arn = module.appsync_app_datasource.invoke_policy_arn
   lambda_function_arn      = module.appsync_app_datasource.arn
   name                     = replace("${var.environment_prefix}_app_datasource", "-", "_")
+  source                   = "QuiNovas/appsync-lambda-datasource/aws"
+  version                  = "3.0.2"
+}
+
+module "message_type_datasource" {
+  api_id                   = aws_appsync_graphql_api.hl7_ninja.id
+  description              = "Appsync lambda datasource for appsync-app-datasource function"
+  invoke_lambda_policy_arn = module.appsync_message_type_datasource.invoke_policy_arn
+  lambda_function_arn      = module.appsync_message_type_datasource.arn
+  name                     = replace("${var.environment_prefix}_message_type_datasource", "-", "_")
   source                   = "QuiNovas/appsync-lambda-datasource/aws"
   version                  = "3.0.2"
 }
