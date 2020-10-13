034a09051c6b993e4268ad916659538ccc637f9a
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-13 20:12:05 +0530

    iron out iam policy

--- lambda.tf
+++ lambda.tf
@@ -1990,22 +1990,27 @@ data "aws_iam_policy_document" "presign_large_messages" {
 
     resources = [
       "${module.large_messages_bucket_us_east_1.arn}/*",
-      module.large_messages_bucket_us_east_1.arn,
+      "${module.large_messages_bucket_us_east_2.arn}/*",
+      "${module.large_messages_bucket_us_west_1.arn}/*",
+      "${module.large_messages_bucket_us_west_2.arn}/*",
     ]
 
     sid = "LargeMessagesStorageAccess"
   }
 
-    statement {
+  statement {
     actions = [
       "kms:decrypt",
     ]
 
     resources = [
-     aws_kms_key.kms_us_east_1.arn
+      aws_kms_key.kms_us_east_1.arn,
+      aws_kms_key.kms_us_east_2.arn,
+      aws_kms_key.kms_us_west_1.arn,
+      aws_kms_key.kms_us_west_2.arn
     ]
 
-    sid = "KMSDecrypt"
+    sid = "DecryptEnvKMSKeys"
   }
 }
 
