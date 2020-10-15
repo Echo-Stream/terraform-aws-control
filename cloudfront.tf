26a1e6156c381252f03f33931cad7ab37aef4420
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-15 20:22:40 +0530

    add source-code-hash

--- cloudfront.tf
+++ cloudfront.tf
@@ -92,16 +92,16 @@ resource "aws_lambda_function" "edge_config" {
     data.archive_file.edge_config
   ]
 
-  description   = "Returns an environment specific config for reactjs application"
-  provider      = aws.us-east-1
-  filename      = "edge-config.zip"
-  function_name = "${var.environment_prefix}-edge-config"
-  handler       = "function.lambda_handler"
-  publish       = true
-  role          = aws_iam_role.edge_config.arn
-  runtime       = "python3.8"
-  #source_code_hash = filebase64sha256(data.template_file.edge_config_py.rendered)
-  tags = local.tags
+  description      = "Returns an environment specific config for reactjs application"
+  provider         = aws.us-east-1
+  filename         = "edge-config.zip"
+  function_name    = "${var.environment_prefix}-edge-config"
+  handler          = "function.lambda_handler"
+  publish          = true
+  role             = aws_iam_role.edge_config.arn
+  runtime          = "python3.8"
+  source_code_hash = data.archive_file.edge_config.output_base64sha256
+  tags             = local.tags
 }
 
 resource "aws_lambda_permission" "edge_config" {
