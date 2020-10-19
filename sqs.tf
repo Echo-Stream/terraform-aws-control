f615e350619038350f93e4ab8f0a63ab092daaac
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-19 21:50:28 +0530

    Enabled content-based deduplication

--- sqs.tf
+++ sqs.tf
@@ -1,5 +1,6 @@
 resource "aws_sqs_queue" "default_tenant_sqs_queue" {
-  name       = "${var.environment_prefix}_db-stream_DEFAULT_TENANT.fifo"
-  fifo_queue = true
-  tags       = local.tags
+  name                        = "${var.environment_prefix}_db-stream_DEFAULT_TENANT.fifo"
+  content_based_deduplication = "true"
+  fifo_queue                  = true
+  tags                        = local.tags
 }
