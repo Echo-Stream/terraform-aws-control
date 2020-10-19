225d382ee05f45983cc33a9ecd014074fffb1cd2
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-10-19 21:43:21 +0530

    default tenant name change

--- sqs.tf
+++ sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "default_tenant_sqs_queue" {
-  name       = "${var.environment_prefix}_db_stream_DEFAULT_TENANT.fifo"
+  name       = "${var.environment_prefix}_db-stream_DEFAULT_TENANT.fifo"
   fifo_queue = true
   tags       = local.tags
 }
