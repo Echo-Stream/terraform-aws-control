ea7a5574edfec5036ac285fec2cb04c98ccf5f04
krajhansh <krajhansh@quinovas.com>
2020-11-27 17:37:17 +0530

    few changes in alarm

--- alarms.tf
+++ alarms.tf
@@ -1,7 +1,6 @@
-resource "aws_cloudwatch_metric_alarm" "lambda" {
-  alarm_name          = "${var.environment_prefix}-lambda-errors"
+resource "aws_cloudwatch_metric_alarm" "appsync_app_ds_lambda" {
+  alarm_name          = "${var.environment_prefix}-appsync-app-ds-lambda-errors"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  dimensions          = "none"
   evaluation_periods  = "1"
   metric_name         = "Errors"
   namespace           = "AWS/Lambda"
@@ -9,7 +8,10 @@ resource "aws_cloudwatch_metric_alarm" "lambda" {
   statistic           = "Average"
   threshold           = "1"
   actions_enabled     = "true"
-  alarm_actions       = [aws_sns_topic.alerts.arn]
-  ok_actions          = [aws_sns_topic.alerts.arn]
-  alarm_description   = "This metric monitors lambda errors"
+  dimensions = {
+    FunctionName = module.appsync_app_datasource.name
+  }
+  alarm_actions     = [aws_sns_topic.alerts.arn]
+  ok_actions        = [aws_sns_topic.alerts.arn]
+  alarm_description = "This metric monitors lambda errors"
 }
