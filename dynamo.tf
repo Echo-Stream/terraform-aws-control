792d6f2260587f6d36dff0136bbed71d2acf598b
vinay kumar <vchinnakotla>
2021-03-09 19:40:12 -0500

    item syntax fix

--- dynamo.tf
+++ dynamo.tf
@@ -89,7 +89,7 @@ resource "aws_dynamodb_table_item" "default_tenant" {
 
   item = <<ITEM
 {
-  "billingInfo": {},
+  "billingInfo": {"M": {}},
   "dbStreamUrl": {"S": "${aws_sqs_queue.default_tenant_sqs_queue.id}"},
   "name": {"S": "DEFAULT_TENANT"},
   "pk": {"S": "DEFAULT_TENANT"},
