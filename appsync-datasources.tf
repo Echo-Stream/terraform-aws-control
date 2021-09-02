c34075a6340f89d038a4484b7ad8b8fd2d605398
vinay kumar <vchinnakotla@>
2021-09-02 16:53:41 -0400

    add datasource

--- appsync-datasources.tf
+++ appsync-datasources.tf
@@ -1,6 +1,17 @@
 # ###################
 # ### Datasources ###
 # ###################
+
+module "appsync_datasource" {
+  api_id                   = aws_appsync_graphql_api.echostream.id
+  description              = "Main Lambda datasource for the echo-stream API "
+  invoke_lambda_policy_arn = module.appsync_datasource.invoke_policy_arn
+  lambda_function_arn      = module.appsync_datasource.arn
+  name                     = replace("${var.resource_prefix}_appsync_datasource", "-", "_")
+  source                   = "QuiNovas/appsync-lambda-datasource/aws"
+  version                  = "3.0.4"
+}
+
 # module "appsync_kms_key_lambda_datasource" {
 #   api_id                   = aws_appsync_graphql_api.echostream.id
 #   description              = "Lambda datasource that manages SQS/KMS resource and IAM policies from a Dynamodb Stream"
