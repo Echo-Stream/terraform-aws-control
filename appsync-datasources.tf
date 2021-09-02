f202ed5b4f2e025a589a719904efc3159eb7b379
vinay kumar <vchinnakotla@>
2021-09-02 10:43:03 -0400

    remove all datasources and resolvers

--- appsync-datasources.tf
+++ appsync-datasources.tf
@@ -1,149 +1,149 @@
-###################
-### Datasources ###
-###################
-module "appsync_kms_key_lambda_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Lambda datasource that manages SQS/KMS resource and IAM policies from a Dynamodb Stream"
-  invoke_lambda_policy_arn = module.appsync_kms_key_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_kms_key_datasource.arn
-  name                     = replace("${var.resource_prefix}_kms_key_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# ###################
+# ### Datasources ###
+# ###################
+# module "appsync_kms_key_lambda_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Lambda datasource that manages SQS/KMS resource and IAM policies from a Dynamodb Stream"
+#   invoke_lambda_policy_arn = module.appsync_kms_key_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_kms_key_datasource.arn
+#   name                     = replace("${var.resource_prefix}_kms_key_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "appsync_edge_lambda_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for managing edges"
-  invoke_lambda_policy_arn = module.appsync_edge_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_edge_datasource.arn
-  name                     = replace("${var.resource_prefix}_edge_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "appsync_edge_lambda_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for managing edges"
+#   invoke_lambda_policy_arn = module.appsync_edge_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_edge_datasource.arn
+#   name                     = replace("${var.resource_prefix}_edge_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "appsync_graph_table_datasource" {
-  api_id              = aws_appsync_graphql_api.echostream.id
-  description         = "graph-table dynamodb datasource"
-  dynamodb_table_arn  = module.graph_table.arn
-  dynamodb_table_name = module.graph_table.name
-  name                = replace("${var.resource_prefix}_dynamodb", "-", "_")
-  source              = "QuiNovas/appsync-dynamodb-datasource/aws"
-  version             = "3.0.4"
-}
+# module "appsync_graph_table_datasource" {
+#   api_id              = aws_appsync_graphql_api.echostream.id
+#   description         = "graph-table dynamodb datasource"
+#   dynamodb_table_arn  = module.graph_table.arn
+#   dynamodb_table_name = module.graph_table.name
+#   name                = replace("${var.resource_prefix}_dynamodb", "-", "_")
+#   source              = "QuiNovas/appsync-dynamodb-datasource/aws"
+#   version             = "3.0.4"
+# }
 
-resource "aws_appsync_datasource" "none" {
-  api_id = aws_appsync_graphql_api.echostream.id
-  name   = "none"
-  type   = "NONE"
-}
+# resource "aws_appsync_datasource" "none" {
+#   api_id = aws_appsync_graphql_api.echostream.id
+#   name   = "none"
+#   type   = "NONE"
+# }
 
-module "validate_function_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for Validate function"
-  invoke_lambda_policy_arn = module.appsync_validate_function_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_validate_function_datasource.arn
-  name                     = replace("${var.resource_prefix}_validate_function", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "validate_function_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for Validate function"
+#   invoke_lambda_policy_arn = module.appsync_validate_function_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_validate_function_datasource.arn
+#   name                     = replace("${var.resource_prefix}_validate_function", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "tenant_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-tenant-datasource function"
-  invoke_lambda_policy_arn = module.appsync_tenant_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_tenant_datasource.arn
-  name                     = replace("${var.resource_prefix}_tenant_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "tenant_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-tenant-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_tenant_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_tenant_datasource.arn
+#   name                     = replace("${var.resource_prefix}_tenant_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "app_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-app-datasource function"
-  invoke_lambda_policy_arn = module.appsync_app_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_app_datasource.arn
-  name                     = replace("${var.resource_prefix}_app_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "app_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-app-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_app_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_app_datasource.arn
+#   name                     = replace("${var.resource_prefix}_app_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "message_type_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-app-datasource function"
-  invoke_lambda_policy_arn = module.appsync_message_type_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_message_type_datasource.arn
-  name                     = replace("${var.resource_prefix}_message_type_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "message_type_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-app-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_message_type_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_message_type_datasource.arn
+#   name                     = replace("${var.resource_prefix}_message_type_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "sub_field_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-sub-field-datasource function"
-  invoke_lambda_policy_arn = module.appsync_sub_field_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_sub_field_datasource.arn
-  name                     = replace("${var.resource_prefix}_sub_field_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "sub_field_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-sub-field-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_sub_field_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_sub_field_datasource.arn
+#   name                     = replace("${var.resource_prefix}_sub_field_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "large_message_storage_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-large-message-storage-datasource function"
-  invoke_lambda_policy_arn = module.appsync_large_message_storage_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_large_message_storage_datasource.arn
-  name                     = replace("${var.resource_prefix}_large_message_storage_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "large_message_storage_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-large-message-storage-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_large_message_storage_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_large_message_storage_datasource.arn
+#   name                     = replace("${var.resource_prefix}_large_message_storage_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "node_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-node-datasource function"
-  invoke_lambda_policy_arn = module.appsync_node_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_node_datasource.arn
-  name                     = replace("${var.resource_prefix}_node_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "node_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-node-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_node_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_node_datasource.arn
+#   name                     = replace("${var.resource_prefix}_node_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
 
-module "subscription_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-subscription-datasource function"
-  invoke_lambda_policy_arn = module.appsync_subscription_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_subscription_datasource.arn
-  name                     = replace("${var.resource_prefix}_subscription_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "subscription_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-subscription-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_subscription_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_subscription_datasource.arn
+#   name                     = replace("${var.resource_prefix}_subscription_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "function_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-function-datasource function"
-  invoke_lambda_policy_arn = module.appsync_function_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_function_datasource.arn
-  name                     = replace("${var.resource_prefix}_function_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "function_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-function-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_function_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_function_datasource.arn
+#   name                     = replace("${var.resource_prefix}_function_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "api_user_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-api-user-datasource function"
-  invoke_lambda_policy_arn = module.appsync_api_user_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_api_user_datasource.arn
-  name                     = replace("${var.resource_prefix}_api_user_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "api_user_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-api-user-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_api_user_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_api_user_datasource.arn
+#   name                     = replace("${var.resource_prefix}_api_user_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
 
-module "integration_datasource" {
-  api_id                   = aws_appsync_graphql_api.echostream.id
-  description              = "Appsync lambda datasource for appsync-integrations-datasource function"
-  invoke_lambda_policy_arn = module.appsync_integrations_datasource.invoke_policy_arn
-  lambda_function_arn      = module.appsync_integrations_datasource.arn
-  name                     = replace("${var.resource_prefix}__integrations_datasource", "-", "_")
-  source                   = "QuiNovas/appsync-lambda-datasource/aws"
-  version                  = "3.0.4"
-}
+# module "integration_datasource" {
+#   api_id                   = aws_appsync_graphql_api.echostream.id
+#   description              = "Appsync lambda datasource for appsync-integrations-datasource function"
+#   invoke_lambda_policy_arn = module.appsync_integrations_datasource.invoke_policy_arn
+#   lambda_function_arn      = module.appsync_integrations_datasource.arn
+#   name                     = replace("${var.resource_prefix}__integrations_datasource", "-", "_")
+#   source                   = "QuiNovas/appsync-lambda-datasource/aws"
+#   version                  = "3.0.4"
+# }
--- appsync-resolvers.tf
+++ appsync-resolvers.tf
@@ -1,63 +1,63 @@
-# We are using custom local exec resolvers,
-# as aws_appsync_resolver TF resource is behind.
-# TF resource doesn't support optional templates yet.
-# https://github.com/terraform-providers/terraform-provider-aws/issues/15593
+# # We are using custom local exec resolvers,
+# # as aws_appsync_resolver TF resource is behind.
+# # TF resource doesn't support optional templates yet.
+# # https://github.com/terraform-providers/terraform-provider-aws/issues/15593
 
-data "template_file" "resolver_sh" {
-  template = file("${path.module}/scripts/resolvers.sh")
-  vars = {
-    api_id                           = aws_appsync_graphql_api.echostream.id
-    api_user_datasource              = module.api_user_datasource.name
-    app_datasource                   = module.app_datasource.name
-    edge_datasource                  = module.appsync_edge_lambda_datasource.name
-    function_datasource              = module.function_datasource.name
-    integration_datasource           = module.integration_datasource.name
-    kms_key_datasource               = module.appsync_kms_key_lambda_datasource.name
-    large_message_storage_datasource = module.large_message_storage_datasource.name
-    message_type_datasource          = module.message_type_datasource.name
-    node_datasource                  = module.node_datasource.name
-    sub_field_datasource             = module.sub_field_datasource.name
-    subscription_datasource          = module.subscription_datasource.name
-    templates_path                   = "${path.module}/files/response-template.vtl"
-    tenant_datasource                = module.tenant_datasource.name
-    validate_function_datasource     = module.validate_function_datasource.name
-  }
-}
+# data "template_file" "resolver_sh" {
+#   template = file("${path.module}/scripts/resolvers.sh")
+#   vars = {
+#     api_id                           = aws_appsync_graphql_api.echostream.id
+#     api_user_datasource              = module.api_user_datasource.name
+#     app_datasource                   = module.app_datasource.name
+#     edge_datasource                  = module.appsync_edge_lambda_datasource.name
+#     function_datasource              = module.function_datasource.name
+#     integration_datasource           = module.integration_datasource.name
+#     kms_key_datasource               = module.appsync_kms_key_lambda_datasource.name
+#     large_message_storage_datasource = module.large_message_storage_datasource.name
+#     message_type_datasource          = module.message_type_datasource.name
+#     node_datasource                  = module.node_datasource.name
+#     sub_field_datasource             = module.sub_field_datasource.name
+#     subscription_datasource          = module.subscription_datasource.name
+#     templates_path                   = "${path.module}/files/response-template.vtl"
+#     tenant_datasource                = module.tenant_datasource.name
+#     validate_function_datasource     = module.validate_function_datasource.name
+#   }
+# }
 
-resource "null_resource" "all_resolvers" {
-  depends_on = [
-    data.template_file.resolver_sh,
-    module.api_user_datasource.name,
-    module.appsync_edge_lambda_datasource,
-    module.appsync_kms_key_lambda_datasource,
-    module.function_datasource,
-    module.integration_datasource,
-    module.large_message_storage_datasource,
-    module.message_type_datasource,
-    module.node_datasource,
-    module.sub_field_datasource,
-    module.subscription_datasource,
-    module.tenant_datasource,
-    module.validate_function_datasource,
-  ]
+# resource "null_resource" "all_resolvers" {
+#   depends_on = [
+#     data.template_file.resolver_sh,
+#     module.api_user_datasource.name,
+#     module.appsync_edge_lambda_datasource,
+#     module.appsync_kms_key_lambda_datasource,
+#     module.function_datasource,
+#     module.integration_datasource,
+#     module.large_message_storage_datasource,
+#     module.message_type_datasource,
+#     module.node_datasource,
+#     module.sub_field_datasource,
+#     module.subscription_datasource,
+#     module.tenant_datasource,
+#     module.validate_function_datasource,
+#   ]
 
-  provisioner "local-exec" {
-    command = "./ ${data.template_file.resolver_sh.rendered}"
-  }
-  triggers = {
-    api_id                           = aws_appsync_graphql_api.echostream.id
-    api_user_datasource              = module.api_user_datasource.name
-    app_datasource                   = module.app_datasource.name
-    deploy                           = data.template_file.resolver_sh.rendered
-    edge_datasource                  = module.appsync_edge_lambda_datasource.name
-    function_datasource              = module.function_datasource.name
-    integration_datasource           = module.integration_datasource.name
-    large_message_storage_datasource = module.large_message_storage_datasource.name
-    message_type_datasource          = module.message_type_datasource.name
-    node_datasource                  = module.node_datasource.name
-    sub_field_datasource             = module.sub_field_datasource.name
-    templates_path                   = "${path.module}/files/response-template.vtl"
-    tenant_datasource                = module.tenant_datasource.name
-    validate_function_datasource     = module.validate_function_datasource.name
-  }
-}
+#   provisioner "local-exec" {
+#     command = "./ ${data.template_file.resolver_sh.rendered}"
+#   }
+#   triggers = {
+#     api_id                           = aws_appsync_graphql_api.echostream.id
+#     api_user_datasource              = module.api_user_datasource.name
+#     app_datasource                   = module.app_datasource.name
+#     deploy                           = data.template_file.resolver_sh.rendered
+#     edge_datasource                  = module.appsync_edge_lambda_datasource.name
+#     function_datasource              = module.function_datasource.name
+#     integration_datasource           = module.integration_datasource.name
+#     large_message_storage_datasource = module.large_message_storage_datasource.name
+#     message_type_datasource          = module.message_type_datasource.name
+#     node_datasource                  = module.node_datasource.name
+#     sub_field_datasource             = module.sub_field_datasource.name
+#     templates_path                   = "${path.module}/files/response-template.vtl"
+#     tenant_datasource                = module.tenant_datasource.name
+#     validate_function_datasource     = module.validate_function_datasource.name
+#   }
+# }
