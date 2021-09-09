3205946a8c8276ff067b3d9633b5d097003048c2
vinay kumar <vchinnakotla@>
2021-09-09 10:03:31 -0400

    move echo-tools env vars into common

--- lambda-graph.tf
+++ lambda-graph.tf
@@ -391,26 +391,7 @@ module "graph_table_tenant_stream_handler" {
   description     = "Delegates calls to handling lambda functions in EchoStream Dynamodb Stream"
   dead_letter_arn = local.lambda_dead_letter_arn
 
-  environment_variables = {
-    #INTERNAL_APPSYNC_ROLES   = local.internal_appsync_role_names
-    #TENANT_DB_STREAM_HANDLER = "echo-dev-graph-table-tenant-stream-handler"
-    #TYPE_HANDLERS            = file("${path.module}/files/type-handlers-map.json")
-    //ALERT_EMITTER_NODE_CODE  = "{\"S3Key\": \"${local.artifacts["lambda"]}/validate-function.zip\"}"
-    ALARM_SNS_TOPIC    = aws_sns_topic.alarms.arn
-    ALERT_SNS_TOPIC    = aws_sns_topic.alerts.arn
-    APPSYNC_ENDPOINT   = aws_appsync_graphql_api.echostream.uris["GRAPHQL"]
-    ARTIFACTS_BUCKET   = local.artifacts_bucket_prefix
-    AUDIT_FIREHOSE     = aws_kinesis_firehose_delivery_stream.process_audit_record_firehose.name
-    CONTROL_REGION     = local.current_region
-    DYNAMODB_TABLE     = module.graph_table.name
-    ENVIRONMENT        = var.resource_prefix
-    ID_TOKEN_KEY       = local.id_token_key
-    INTERNAL_NODE_CODE = "{\"S3Key\": \"${local.artifacts["tenant_lambda"]}/internal-node.zip\"}"
-    INTERNAL_NODE_ROLE = aws_iam_role.tenant_function.name
-    UPDATE_CODE_ROLE   = aws_iam_role.update_code.arn
-    VALIDATOR_CODE     = "{\"S3Key\": \"${local.artifacts["lambda"]}/validator.zip\"}"
-    VALIDATOR_ROLE     = aws_iam_role.validator.arn
-  }
+  environment_variables = local.common_lambda_environment_variables
 
   handler     = "function.handler"
   kms_key_arn = local.lambda_env_vars_kms_key_arn
