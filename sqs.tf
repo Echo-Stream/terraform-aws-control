7896057ddb3c0398f64d28c6469f5457b559d083
vinay kumar <vchinnakotla@>
2021-09-13 18:50:06 -0400

    add encryption to system queue

--- sqs.tf
+++ sqs.tf
@@ -1,7 +1,8 @@
 resource "aws_sqs_queue" "system_sqs_queue" {
-  name                        = "${var.resource_prefix}_system-db-stream.fifo"
   content_based_deduplication = "true"
   fifo_queue                  = true
+  kms_master_key_id           = "alias/aws/sqs"
+  name                        = "${var.resource_prefix}_system-db-stream.fifo"
   tags                        = local.tags
   visibility_timeout_seconds  = 300
 }
