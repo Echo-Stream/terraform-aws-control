fc8aac6c2de27c80664501306102b86aada032f7
vinay kumar <vchinnakotla@>
2021-09-14 14:07:26 -0400

    correct topic name

--- lambda-appsync.tf
+++ lambda-appsync.tf
@@ -57,7 +57,7 @@ module "appsync_datasource" {
       APP_IDENTITY_POOL_ID         = aws_cognito_identity_pool.echostream.id
       APP_USER_POOL_APP_ID         = aws_cognito_user_pool_client.echostream_apps_userpool_client.id
       APP_USER_POOL_ID             = aws_cognito_user_pool.echostream_apps.id
-      MANAGED_APP_CLOUD_INIT_TOPIC = aws_sns_topic.hl7_app_cloud_init.arn
+      MANAGED_APP_CLOUD_INIT_TOPIC = aws_sns_topic.managed_app_cloud_init.arn
       REMOTE_APP_ROLE              = ""
       SSM_SERVICE_ROLE             = "service-role/${aws_iam_role.manage_apps_ssm_service_role.name}"
       UI_USER_POOL_ID              = aws_cognito_user_pool.echostream_ui.id
--- sns.tf
+++ sns.tf
@@ -1,12 +1,12 @@
-resource "aws_sns_topic" "hl7_app_cloud_init" {
-  name         = "${var.resource_prefix}-hl7-app-cloud-init"
-  display_name = "HL7 App Cloud Init topic"
+resource "aws_sns_topic" "managed_app_cloud_init" {
+  name         = "${var.resource_prefix}-managed-app-cloud-init"
+  display_name = "Managed App Cloud Init topic"
   tags         = local.tags
 }
 
-module "hl7_app_cloud_init_subscription" {
+module "managed_app_cloud_init_subscription" {
   install_aws_cli = false
-  topic_arn       = aws_sns_topic.hl7_app_cloud_init.arn
+  topic_arn       = aws_sns_topic.managed_app_cloud_init.arn
   email_address   = var.ses_email_address
   source          = "QuiNovas/sns-email-subscription/aws"
   version         = "0.0.2"
