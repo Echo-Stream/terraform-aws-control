36ccfebb044bc4da30029bdbe6880bf785d336c7
vinay kumar <vchinnakotla@>
2021-11-01 12:33:28 -0400

    system and tenant stream handler iam updates

--- lambda-graph.tf
+++ lambda-graph.tf
@@ -190,11 +190,10 @@ data "aws_iam_policy_document" "graph_table_tenant_stream_handler" {
   statement {
     actions = [
       "sqs:DeleteQueue",
-      "sqs:GetQueueUrl",
     ]
 
     resources = [
-      "arn:aws:sqs:*:data.aws_caller_identity.current.account_id:edge*.fifo"
+      "arn:aws:sqs:*:${data.aws_caller_identity.current.account_id}:edge*.fifo"
     ]
 
     sid = "DeleteQueue"
@@ -442,6 +441,17 @@ resource "aws_cloudwatch_log_subscription_filter" "graph_table_tenant_stream_han
 ## graph-table-system-stream-handler ##
 #######################################
 data "aws_iam_policy_document" "graph_table_system_stream_handler" {
+    statement {
+    actions = [
+      "sqs:DeleteQueue",
+    ]
+
+    resources = [
+      "arn:aws:sqs:*:${data.aws_caller_identity.current.account_id}:db-stream*.fifo"
+    ]
+
+    sid = "DeleteQueue"
+  }
   statement {
     actions = [
       "dynamodb:*",
