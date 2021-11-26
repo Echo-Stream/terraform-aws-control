1098b7a112b51f327b9eda1f29765a2ccbec4a78
vinay kumar <vchinnakotla@>
2021-11-26 14:01:52 -0500

    add bulk data user keys to appsync datasource

--- lambda-appsync.tf
+++ lambda-appsync.tf
@@ -55,13 +55,15 @@ module "appsync_datasource" {
       # APP_USER_POOL_APP_ID         = aws_cognito_user_pool_client.echostream_apps_userpool_client.id
       # APP_USER_POOL_ID             = aws_cognito_user_pool.echostream_apps.id
       #APP_IDENTITY_POOL_ID         = aws_cognito_identity_pool.echostream.id
-      API_USER_POOL_CLIENT_ID       = aws_cognito_user_pool_client.echostream_api_userpool_client.id
-      API_USER_POOL_ID              = aws_cognito_user_pool.echostream_api.id
-      MANAGED_APP_CLOUD_INIT_TOPIC  = aws_sns_topic.managed_app_cloud_init.arn
-      REMOTE_APP_ROLE               = aws_iam_role.remote_app.arn
-      SSM_SERVICE_ROLE              = "service-role/${aws_iam_role.managed_app.name}"
-      TENANT_DB_STREAM_HANDLER_ROLE = module.graph_table_tenant_stream_handler.role_arn
-      UI_USER_POOL_ID               = aws_cognito_user_pool.echostream_ui.id
+      API_USER_POOL_CLIENT_ID         = aws_cognito_user_pool_client.echostream_api_userpool_client.id
+      API_USER_POOL_ID                = aws_cognito_user_pool.echostream_api.id
+      BULK_DATA_AWS_ACCESS_KEY_ID     = aws_iam_access_key.presign_bulk_data.id
+      BULK_DATA_AWS_SECRET_ACCESS_KEY = aws_iam_access_key.presign_bulk_data.secret
+      MANAGED_APP_CLOUD_INIT_TOPIC    = aws_sns_topic.managed_app_cloud_init.arn
+      REMOTE_APP_ROLE                 = aws_iam_role.remote_app.arn
+      SSM_SERVICE_ROLE                = "service-role/${aws_iam_role.managed_app.name}"
+      TENANT_DB_STREAM_HANDLER_ROLE   = module.graph_table_tenant_stream_handler.role_arn
+      UI_USER_POOL_ID                 = aws_cognito_user_pool.echostream_ui.id
     }
   )
 
