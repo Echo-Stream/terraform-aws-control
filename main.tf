9bf7b0b4619f94899a9d7929fb133f19b0fcd414
vinay kumar <vchinnakotla@>
2021-12-09 17:31:13 -0500

    update all log buckets

--- main.tf
+++ main.tf
@@ -37,10 +37,11 @@ module "log_bucket_us_east_1" {
 }
 
 module "log_bucket_us_east_2" {
-  count       = contains(local.regions, "us-east-2") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-us-east-2"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "us-east-2") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "us-east-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.ohio
@@ -48,10 +49,11 @@ module "log_bucket_us_east_2" {
 }
 
 module "log_bucket_us_west_1" {
-  count       = contains(local.regions, "us-west-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-us-west-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "us-west-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "us-west-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.north-california
@@ -59,10 +61,11 @@ module "log_bucket_us_west_1" {
 }
 
 module "log_bucket_us_west_2" {
-  count       = contains(local.regions, "us-west-2") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-us-west-2"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "us-west-2") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "us-west-2"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.oregon
@@ -71,10 +74,11 @@ module "log_bucket_us_west_2" {
 
 ####
 module "log_bucket_af_south_1" {
-  count       = contains(local.regions, "af-south-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-af-south-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "af-south-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "af-south-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.cape-town
@@ -83,10 +87,11 @@ module "log_bucket_af_south_1" {
 
 ####
 module "log_bucket_ap_east_1" {
-  count       = contains(local.regions, "ap-east-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ap-east-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ap-east-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ap-east-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.hongkong
@@ -94,10 +99,11 @@ module "log_bucket_ap_east_1" {
 }
 
 module "log_bucket_ap_south_1" {
-  count       = contains(local.regions, "ap-south-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ap-south-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ap-south-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ap-south-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.mumbai
@@ -105,10 +111,11 @@ module "log_bucket_ap_south_1" {
 }
 
 module "log_bucket_ap_northeast_2" {
-  count       = contains(local.regions, "ap-northeast-2") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ap-northeast-2"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ap-northeast-2") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ap-northeast-2"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.osaka
@@ -116,10 +123,11 @@ module "log_bucket_ap_northeast_2" {
 }
 
 module "log_bucket_ap_southeast_1" {
-  count       = contains(local.regions, "ap-southeast-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ap-southeast-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ap-southeast-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ap-southeast-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.singapore
@@ -127,10 +135,11 @@ module "log_bucket_ap_southeast_1" {
 }
 
 module "log_bucket_ap_southeast_2" {
-  count       = contains(local.regions, "ap-southeast-2") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ap-southeast-2"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ap-southeast-2") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ap-southeast-2"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.sydney
@@ -138,10 +147,11 @@ module "log_bucket_ap_southeast_2" {
 }
 
 module "log_bucket_ap_northeast_1" {
-  count       = contains(local.regions, "ap-northeast-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ap-northeast-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ap-northeast-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ap-northeast-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.tokyo
@@ -150,10 +160,11 @@ module "log_bucket_ap_northeast_1" {
 
 ####
 module "log_bucket_ca_central_1" {
-  count       = contains(local.regions, "ca-central-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-ca-central-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "ca-central-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "ca-central-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.central
@@ -162,10 +173,11 @@ module "log_bucket_ca_central_1" {
 
 ####
 module "log_bucket_eu_central_1" {
-  count       = contains(local.regions, "eu-central-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-eu-central-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "eu-central-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "eu-central-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.frankfurt
@@ -173,10 +185,11 @@ module "log_bucket_eu_central_1" {
 }
 
 module "log_bucket_eu_west_1" {
-  count       = contains(local.regions, "eu-west-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-eu-west-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "eu-west-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "eu-west-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.ireland
@@ -184,10 +197,11 @@ module "log_bucket_eu_west_1" {
 }
 
 module "log_bucket_eu_west_2" {
-  count       = contains(local.regions, "eu-west-2") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-eu-west-2"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "eu-west-2") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "eu-west-2"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.london
@@ -195,10 +209,11 @@ module "log_bucket_eu_west_2" {
 }
 
 module "log_bucket_eu_south_1" {
-  count       = contains(local.regions, "eu-south-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-eu-south-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "eu-south-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "eu-south-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.milan
@@ -206,10 +221,11 @@ module "log_bucket_eu_south_1" {
 }
 
 module "log_bucket_eu_west_3" {
-  count       = contains(local.regions, "eu-west-3") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-eu-west-3"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "eu-west-3") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "eu-west-3"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.paris
@@ -217,10 +233,11 @@ module "log_bucket_eu_west_3" {
 }
 
 module "log_bucket_eu_north_1" {
-  count       = contains(local.regions, "eu-north-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-eu-north-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "eu-north-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "eu-north-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.stockholm
@@ -229,10 +246,11 @@ module "log_bucket_eu_north_1" {
 
 ####
 module "log_bucket_me_south_1" {
-  count       = contains(local.regions, "me-south-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-me-south-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "me-south-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "me-south-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.bahrain
@@ -241,31 +259,13 @@ module "log_bucket_me_south_1" {
 
 ####
 module "log_bucket_sa_east_1" {
-  count       = contains(local.regions, "sa-east-1") == true ? 1 : 0
-  name_prefix = "${var.resource_prefix}-sa-east-1"
-  source      = "QuiNovas/log-bucket/aws"
-  version     = "3.0.2"
+  count        = contains(local.regions, "sa-east-1") == true ? 1 : 0
+  name_prefix  = var.resource_prefix
+  name_postfix = "sa-east-1"
+  source       = "QuiNovas/log-bucket/aws"
+  version      = "3.0.5"
 
   providers = {
     aws = aws.sao-paulo
   }
 }
-
-# ## Trigger Deployment
-# resource "null_resource" "echo_deployment" {
-#   depends_on = [
-#     module.graph_table,
-#     module.deployment_handler,
-#     aws_appsync_graphql_api.echostream,
-#     aws_cloudfront_distribution.webapp,
-#   ]
-
-#   triggers = {
-#     version        = var.echostream_version
-#     manual_trigger = var.manual_deployment_trigger
-#   }
-
-#   provisioner "local-exec" {
-#     command = "aws lambda invoke --function-name ${module.deployment_handler.name} --invocation-type Event --payload '{\"trigger\": \"terraform\"}' response.json"
-#   }
-# }
