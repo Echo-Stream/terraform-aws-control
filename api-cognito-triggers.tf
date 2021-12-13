574734e050a31233f312462a6aa8e94265f0931f
vinay kumar <vchinnakotla@>
2021-12-13 12:30:42 -0500

    test

--- api-cognito-triggers.tf
+++ api-cognito-triggers.tf
@@ -306,7 +306,7 @@ module "ui_cognito_preview_pre_signup" {
   description = "Validate invitation for new UI user "
 
   environment_variables = {
-    AUTHORIZED_DOMAINS = "['echo.stream']"
+    AUTHORIZED_DOMAINS = jsonencode(["echo.stream"])
     CONTROL_REGION     = local.current_region
     DYNAMODB_TABLE     = module.graph_table.name
     ENVIRONMENT        = var.resource_prefix
