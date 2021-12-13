ca18a46339644dd3f9b911a85558db147d1db527
vinay kumar <vchinnakotla@>
2021-12-13 15:59:13 -0500

    add env vars

--- api-cognito-triggers.tf
+++ api-cognito-triggers.tf
@@ -191,7 +191,11 @@ module "ui_cognito_pre_signup" {
 
   environment_variables = {
     AUTHORIZED_DOMAINS = jsonencode(["echo.stream"])
+    CHECK_DOMAINS      = true
+    CONTROL_REGION     = local.current_region
+    DYNAMODB_TABLE     = module.graph_table.name
     ENVIRONMENT        = var.resource_prefix
+    TENANT_REGIONS     = jsonencode(local.tenant_regions)
   }
 
   dead_letter_arn = local.lambda_dead_letter_arn
