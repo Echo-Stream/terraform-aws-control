c86f15045d7533c2cc04f870e954ee25340968e4
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-01-13 16:57:03 -0500

    add iam permissions to presign bulk user

--- bulk-data.tf
+++ bulk-data.tf
@@ -1,3 +1,43 @@
+data "aws_iam_policy_document" "presign_bulk_data" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.resource_prefix}-tenant-*",
+    ]
+
+    sid = "PutObjectsInTenantBuckets"
+  }
+
+  statement {
+    actions = [
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKey*",
+    ]
+
+    resources = [
+      "arn:aws:kms:${var.region}:${var.allowed_account_id}:key/*"
+    ]
+
+    sid = "EncryptDataUsingTenantKeys"
+  }
+}
+
+resource "aws_iam_policy" "presign_bulk_data" {
+  description = "IAM permissions required for presign-bulk-data"
+  name        = "${var.resource_prefix}-presign-bulk-data"
+  policy      = data.aws_iam_policy_document.presign_bulk_data.json
+}
+
+resource "aws_iam_user_policy_attachment" "presign_bulk_data" {
+  user       = aws_iam_user.presign_bulk_data.name
+  policy_arn = aws_iam_policy.presign_bulk_data.arn
+}
+
 resource "aws_iam_user" "presign_bulk_data" {
   name = "${var.resource_prefix}-presign-bulk-data"
   tags = local.tags
