3f53f08dfbe2295d812460e6ff5a72e597273229
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-02-12 18:03:28 -0500

    remove unused local var

--- locals.tf
+++ locals.tf
@@ -68,7 +68,6 @@ locals {
   }
 
   log_bucket             = module.log_bucket.id
-  module_release_version = "0.0.3"                                    # this is used in workflows/publish.yml to release terraform module
   regions                = concat(local.tenant_regions, [var.region]) # Tenant + Control Regions
   tenant_regions         = split(",", var.tenant_regions)             # only Tenant regions
 
