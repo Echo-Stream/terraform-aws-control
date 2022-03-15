d4d4cadd30381843cf2b48aedbebb864f462a342
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-03-15 14:07:29 -0400

    terraform init

--- cognito-userpool-app.tf
+++ cognito-userpool-app.tf
@@ -1,5 +1,5 @@
 module "lambda_underpin_us_east_2" {
-  name = local.resource_prefix
+  name = var.resource_prefix
   tags = local.tags
 
   source = "./_modules/lambda-underpin"
@@ -9,20 +9,20 @@ module "lambda_underpin_us_east_2" {
   }
 }
 
-################################
-## App Cognito Pool us-east-2 ##
-################################
+# ###############################
+# # App Cognito Pool us-east-2 ##
+# ###############################
 # module "app_cognito_pool_us_east_2" {
 #   count = contains(local.regions, "us-east-2") == true ? 1 : 0
 
+
 #   artifacts_bucket = local.artifacts_bucket
-#   control_region = local.current_region
+#   control_region   = local.current_region
 #   environment      = var.resource_prefix
 #   graph_table_name = module.graph_table.name
 #   name             = "${local.resource_prefix}-us-east-2"
 #   tags             = local.tags
 #   tenant_regions   = jsonencode(local.tenant_regions)
-
 #   providers = {
 #     aws = aws.us-east-2
 #   }
