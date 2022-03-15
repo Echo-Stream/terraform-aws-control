caf2dc5ab52002b1baf975080fff550b44044b21
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-03-15 17:31:21 -0400

    fix us-east-1 resource

--- cognito-userpool-app.tf
+++ cognito-userpool-app.tf
@@ -1,17 +1,17 @@
 ###############################
 # App Cognito Pool us-east-2 ##
 ###############################
-module "app_cognito_pool_us_east_2" {
-  count = contains(local.regions, "us-east-2") == true ? 1 : 0
+module "app_cognito_pool_us_east_1" {
+  count = contains(local.regions, "us-east-1") == true ? 1 : 0
 
-  artifacts_bucket       = "${local.artifacts_bucket_prefix}-us-east-2"
+  artifacts_bucket       = "${local.artifacts_bucket_prefix}-us-east-1"
   control_region         = local.current_region
   dead_letter_arn        = aws_sns_topic.lambda_dead_letter.arn
   environment            = var.resource_prefix
   function_s3_object_key = local.lambda_functions_keys["app_api_cognito_pre_authentication"]
   graph_table_name       = module.graph_table.name
   kms_key_arn            = aws_kms_key.lambda_environment_variables.kms_key_arn
-  name                   = "${var.resource_prefix}-us-east-2"
+  name                   = "${var.resource_prefix}-us-east-1"
   tags                   = local.tags
   tenant_regions         = jsonencode(local.tenant_regions)
 
