a515a2fa03939909eb1c7df3420b18ae54d31161
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-03-16 15:44:40 -0400

    test in one more region

--- cognito-userpool-app.tf
+++ cognito-userpool-app.tf
@@ -16,7 +16,7 @@ module "app_cognito_pool_us_east_1" {
   tags                                           = local.tags
   tenant_region                                  = "us-east-1"
   tenant_regions                                 = jsonencode(local.tenant_regions)
-  
+
   source = "./_modules/app-user-pool"
 
   providers = {
@@ -24,32 +24,32 @@ module "app_cognito_pool_us_east_1" {
   }
 }
 
-# ###############################
-# # App Cognito Pool us-east-2 ##
-# ###############################
-# module "app_cognito_pool_us_east_2" {
-#   count = contains(local.regions, "us-east-2") == true ? 1 : 0
+################################
+## App Cognito Pool us-east-2 ##
+################################
+module "app_cognito_pool_us_east_2" {
+  count = contains(local.regions, "us-east-2") == true ? 1 : 0
 
-#   app_cognito_pre_authentication_iam_policy_arn = aws_iam_policy.app_api_cognito_pre_authentication.arn
-#   artifacts_bucket                              = "${local.artifacts_bucket_prefix}-us-east-2"
-#   control_region                                = local.current_region
-#   dead_letter_arn                               = module.lambda_underpin_us_east_2.dead_letter_arn
-#   environment                                   = var.resource_prefix
-#   function_s3_object_key                        = local.lambda_functions_keys["app_api_cognito_pre_authentication"]
-#   graph_ddb_read_iam_policy_arn                 = aws_iam_policy.graph_ddb_read.arn
-#   graph_table_name                              = module.graph_table.name
-#   kms_key_arn                                   = module.lambda_underpin_us_east_2.kms_key_arn
-#   name                                          = var.resource_prefix
-#   tags                                          = local.tags
-#   tenant_region                                 = "us-east-2"
-#   tenant_regions                                = jsonencode(local.tenant_regions)
+  app_cognito_pre_authentication_lambda_role_arn = aws_iam_role.app_cognito_pre_authentication_function.arn
+  artifacts_bucket                               = "${local.artifacts_bucket_prefix}-us-east-2"
+  control_region                                 = local.current_region
+  dead_letter_arn                                = aws_sns_topic.lambda_dead_letter.arn
+  environment                                    = var.resource_prefix
+  function_s3_object_key                         = local.lambda_functions_keys["app_api_cognito_pre_authentication"]
+  graph_table_name                               = module.graph_table.name
+  kms_key_arn                                    = aws_kms_key.lambda_environment_variables.arn
+  name                                           = var.resource_prefix
+  tags                                           = local.tags
+  tenant_region                                  = "us-east-2"
+  tenant_regions                                 = jsonencode(local.tenant_regions)
 
-#   source = "./_modules/app-user-pool"
+  source = "./_modules/app-user-pool"
 
-#   providers = {
-#     aws = aws.ohio
-#   }
-# }
+  providers = {
+    aws = aws.ohio
+  }
+}
+
 
 # ###############################
 # # App Cognito Pool us-west-1 ##
