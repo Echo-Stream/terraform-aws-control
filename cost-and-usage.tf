467fe7664068cac3607d4d79108278255c98ee3b
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-04-04 16:08:51 -0400

    add cost and usage definitation

--- cost-and-usage.tf
+++ cost-and-usage.tf
@@ -70,9 +70,43 @@ data "aws_iam_policy_document" "cost_and_usage" {
     ]
     sid = "DenyUnsecuredTransport"
   }
+
+  statement {
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetBucketPolicy",
+      "s3:PutObject"
+    ]
+    effect = "Allow"
+    principals {
+      identifiers = [
+        "billingreports.amazonaws.com",
+      ]
+      type = "Service"
+    }
+    resources = [
+      aws_s3_bucket.cost_and_usage.arn,
+      "${aws_s3_bucket.cost_and_usage.arn}/*",
+    ]
+    sid = "Billing Reports"
+  }
 }
 
 resource "aws_s3_bucket_policy" "cost_and_usage" {
   bucket = aws_s3_bucket.cost_and_usage.id
   policy = data.aws_iam_policy_document.cost_and_usage.json
+}
+
+## CUR Report Definition ##
+resource "aws_cur_report_definition" "cost_and_usage" {
+  additional_artifacts       = ["REDSHIFT", "QUICKSIGHT"]
+  additional_schema_elements = ["RESOURCES"]
+  compression                = "Parquet"
+  format                     = "Parquet"
+  refresh_closed_reports     = true
+  report_name                = "CostAndUsage"
+  report_versioning          = "OVERWRITE_REPORT"
+  s3_bucket                  = aws_s3_bucket.cost_and_usage.id
+  s3_region                  = local.current_region
+  time_unit                  = "DAILY"
 }
