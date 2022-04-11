cbe261aaeced61150920dcee63f3c61b68796bc5
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-04-11 10:20:48 -0400

    add all control lambda alarms

--- monitoring.tf
+++ monitoring.tf
@@ -6,6 +6,20 @@ locals {
     aws_sqs_queue.rebuild_notifications.name
   ]
 
+  lambda_names = [
+    module.api_cognito_pre_authentication.name,
+    module.appsync_datasource.name,
+    module.deployment_handler.name,
+    module.graph_table_dynamodb_trigger.name,
+    module.graph_table_system_stream_handler.name,
+    module.graph_table_tenant_stream_handler.name,
+    module.managed_app_cloud_init.name,
+    module.rebuild_notifications.name,
+    module.ui_cognito_post_confirmation.name,
+    module.ui_cognito_pre_authentication.name,
+    module.ui_cognito_pre_signup.name,
+  ]
+
 }
 
 
@@ -35,21 +49,21 @@ resource "aws_cloudwatch_metric_alarm" "sqs" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "lambda" {
-
-  alarm_name          = "test"
+  for_each            = toset(local.lambda_names)
+  alarm_name          = each.key
   alarm_actions       = [aws_sns_topic.alarms.arn]
   comparison_operator = "GreaterThanOrEqualToThreshold"
 
   dimensions = {
-    FunctionName = module.appsync_datasource.name
+    FunctionName = each.key
   }
 
-  evaluation_periods        = "4"
+  evaluation_periods        = "3"
   metric_name               = "Errors"
   namespace                 = "AWS/Lambda"
   period                    = "60"
   statistic                 = "Sum"
-  threshold                 = "120"
+  threshold                 = "1"
   alarm_description         = "Errors > 1"
   insufficient_data_actions = [aws_sns_topic.alarms.arn]
   treat_missing_data        = "notBreaching"
