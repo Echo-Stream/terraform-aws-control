66c2ebdfa2900deb6efc9d71d44c02b433c9932a
Joseph Wortmann <jwortmann@quinovas.com>
2022-11-17 09:36:06 -0600

    ensure archive is correctly included

--- versions.tf
+++ versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">=2.2.0"
+    }
     aws = {
       configuration_aliases = [aws.route-53]
       source                = "hashicorp/aws"
