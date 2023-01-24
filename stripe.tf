13f8db1b097c51215b0f1f3ea8ed11fe622c5e75
Joseph Wortmann <joseph.wortmann@gmail.com>
2023-01-24 17:46:13 +0000

    added new varaibles

--- stripe.tf
+++ stripe.tf
@@ -7,6 +7,8 @@ module "stripe" {
   count = var.stripe_api_key == "" ? 0 : 1
 
   api_key                           = var.stripe_api_key
+  artifacts_bucket                  = local.artifacts_bucket
+  function_s3_object_key            = local.lambda_functions_keys["stripe"]
   resource_prefix                   = var.resource_prefix
   stripe_api_key_secret_name        = local.stripe_api_key_secret_name
   stripe_webhook_secret_secret_name = local.stripe_webhook_secret_secret_name
