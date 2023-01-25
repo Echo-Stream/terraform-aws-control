6108b5e53863b9cde4c41dbc0d24d93973792e2f
Joseph Wortmann <joseph.wortmann@gmail.com>
2023-01-25 17:43:47 +0000

    condition fix

--- stripe.tf
+++ stripe.tf
@@ -1,10 +1,10 @@
 locals {
-  stripe_api_key_secret_name        = "${var.resource_prefix}-stripe-api-key" ? var.stripe_api_key != "" : ""
-  stripe_webhook_secret_secret_name = "${var.resource_prefix}-stripe-webhook-secret" ? var.stripe_api_key != "" : ""
+  stripe_api_key_secret_name        = var.stripe_api_key != "" ? "${var.resource_prefix}-stripe-api-key" : ""
+  stripe_webhook_secret_secret_name = var.stripe_api_key != "" ? "${var.resource_prefix}-stripe-webhook-secret" : ""
 }
 
 module "stripe" {
-  count = var.stripe_api_key == "" ? 0 : 1
+  count = var.stripe_api_key != "" ? 1 : 0
 
   api_key                           = var.stripe_api_key
   artifacts_bucket                  = local.artifacts_bucket
