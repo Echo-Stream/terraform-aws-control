bd0eef88009d8f95a0d853a33e812dd6aa72fd80
Joseph Wortmann <joseph.wortmann@gmail.com>
2024-04-30 18:52:36 +0000

    Update cost-and-usage.tf with AWS BCM data exports configuration

--- cost-and-usage.tf
+++ cost-and-usage.tf
@@ -1,3 +1,44 @@
+resource "aws_bcmdataexports_export" "cost_and_usage" {
+  depends_on  = [aws_s3_bucket_policy.cost_and_usage]
+  description = "EchoStream Billing Exports"
+
+  export {
+    data_query {
+      query_statement = "SELECT * FROM COST_AND_USAGE_REPORT"
+      table_configurations = {
+        COST_AND_USAGE_REPORT = {
+          INCLUDE_MANUAL_DISCOUNT_COMPATIBILITY = "FALSE",
+          INCLUDE_RESOURCES                     = "TRUE",
+          INCLUDE_SPLIT_COST_ALLOCATION_DATA    = "FALSE",
+          TIME_GRANULARITY                      = "MONTHLY",
+        }
+      }
+    }
+
+    destination_configurations {
+      s3_destination {
+        s3_bucket = aws_s3_bucket.cost_and_usage.id
+
+        s3_output_configurations {
+          compression = "PARQUET"
+          format      = "PARQUET"
+          output_type = "CUSTOM"
+          overwrite   = "OVERWRITE_REPORT"
+        }
+
+        s3_prefix = "exports"
+        s3_region = aws_s3_bucket.cost_and_usage.region
+      }
+    }
+
+    name = "CostAndUsage"
+
+    refresh_cadence {
+      frequency = "SYNCHRONOUS"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "cost_and_usage" {
   bucket = "${var.resource_prefix}-cost-and-usage"
 
@@ -8,11 +49,84 @@ resource "aws_s3_bucket" "cost_and_usage" {
   tags = local.tags
 }
 
+resource "aws_s3_bucket_acl" "cost_and_usage" {
+  bucket = aws_s3_bucket.cost_and_usage.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "cost_and_usage" {
+  bucket = aws_s3_bucket.cost_and_usage.id
+
+  rule {
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 1
+    }
+
+    id     = "cost-and-usage-cleanup"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+}
+
 resource "aws_s3_bucket_logging" "cost_and_usage" {
   bucket = aws_s3_bucket.cost_and_usage.id
 
   target_bucket = local.log_bucket
   target_prefix = "${var.resource_prefix}-cost-and-usage/"
+}
+
+data "aws_iam_policy_document" "cost_and_usage" {
+  statement {
+    actions = [
+      "s3:GetBucketPolicy",
+      "s3:PutObject"
+    ]
+    condition {
+      test = "StringLike"
+      values = [
+        aws_caller_identity.current.account_id,
+      ]
+      variable = "aws:SourceAccount"
+    }
+    condition {
+      test = "StringLike"
+      values = [
+        "arn:aws:bcm-data-exports:us-east-1:${aws_caller_identity.current.account_id}:export/*",
+        "arn:aws:cur:us-east-1:${aws_caller_identity.current.account_id}:definition/*",
+      ]
+      variable = "aws:SourceArn"
+    }
+    effect = "Allow"
+    principals {
+      identifiers = [
+        "bcm-data-exports.amazonaws.com",
+        "billingreports.amazonaws.com",
+      ]
+      type = "Service"
+    }
+    resources = [
+      aws_s3_bucket.cost_and_usage.arn,
+      "${aws_s3_bucket.cost_and_usage.arn}/*",
+    ]
+    sid = "CostAndUsageReports"
+  }
+}
+
+resource "aws_s3_bucket_policy" "cost_and_usage" {
+  bucket = aws_s3_bucket.cost_and_usage.id
+  policy = data.aws_iam_policy_document.cost_and_usage.json
+}
+
+resource "aws_s3_bucket_public_access_block" "cost_and_usage" {
+  bucket                  = aws_s3_bucket.cost_and_usage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "cost_and_usage" {
@@ -25,11 +139,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "cost_and_usage" {
   }
 }
 
-resource "aws_s3_bucket_acl" "cost_and_usage" {
-  bucket = aws_s3_bucket.cost_and_usage.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "cost_and_usage" {
   bucket = aws_s3_bucket.cost_and_usage.id
   versioning_configuration {
@@ -37,67 +146,8 @@ resource "aws_s3_bucket_versioning" "cost_and_usage" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "cost_and_usage" {
-  bucket                  = aws_s3_bucket.cost_and_usage.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-data "aws_iam_policy_document" "cost_and_usage" {
-  statement {
-    actions = [
-      "s3:*",
-    ]
-    condition {
-      test = "Bool"
-      values = [
-        "false",
-      ]
-      variable = "aws:SecureTransport"
-    }
-    effect = "Deny"
-    principals {
-      identifiers = [
-        "*",
-      ]
-      type = "AWS"
-    }
-    resources = [
-      aws_s3_bucket.cost_and_usage.arn,
-      "${aws_s3_bucket.cost_and_usage.arn}/*",
-    ]
-    sid = "DenyUnsecuredTransport"
-  }
-
-  statement {
-    actions = [
-      "s3:GetBucketAcl",
-      "s3:GetBucketPolicy",
-      "s3:PutObject"
-    ]
-    effect = "Allow"
-    principals {
-      identifiers = [
-        "billingreports.amazonaws.com",
-      ]
-      type = "Service"
-    }
-    resources = [
-      aws_s3_bucket.cost_and_usage.arn,
-      "${aws_s3_bucket.cost_and_usage.arn}/*",
-    ]
-    sid = "Billing Reports"
-  }
-}
-
-resource "aws_s3_bucket_policy" "cost_and_usage" {
-  bucket = aws_s3_bucket.cost_and_usage.id
-  policy = data.aws_iam_policy_document.cost_and_usage.json
-}
-
 ## CUR Report Definition ##
+/*
 resource "aws_cur_report_definition" "cost_and_usage" {
   depends_on                 = [aws_s3_bucket_policy.cost_and_usage]
   additional_schema_elements = ["RESOURCES"]
@@ -119,3 +169,4 @@ resource "aws_cur_report_definition" "cost_and_usage" {
 
   provider = aws.us-east-1
 }
+*/
