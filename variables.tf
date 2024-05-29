f8bcd73e6f6885611c1961683f999b8fdad8abe4
Joseph Wortmann <joseph.wortmann@gmail.com>
2024-05-29 16:28:06 +0000

    Added bliing variables

--- variables.tf
+++ variables.tf
@@ -1,23 +1,30 @@
 variable "authorized_domains" {
+  default     = []
   description = "List of authorized_domains that can signup to the app"
   type        = list(string)
-  default     = []
+}
+
+variable "billing_enabled" {
+  default     = false
+  description = "True is billing is enabled via Paddle."
+  type        = bool
 }
 
 variable "check_domains" {
+  default     = false
   description = "Controls whether we use authorized domains or not"
   type        = bool
-  default     = false
 }
 
 variable "create_dynamo_db_replication_service_role" {
+  default     = true
   description = "Enable boolean to create a dynamo db replication service role"
   type        = bool
-  default     = true
 }
 
 variable "domain_name" {
   description = "Your root domain, Used to fetch the hosted_zone_id and create webapp/appsync dns"
+  type        = string
 }
 
 variable "echostream_version" {
@@ -27,10 +34,33 @@ variable "echostream_version" {
 
 variable "environment" {
   description = "Environment. Could be dev, stg, prod"
+  type        = string
+}
+
+variable "paddle_api_key" {
+  description = "Paddle API Key"
+  sensitive   = true
+  type        = string
+}
+
+variable "paddle_client_side_token" {
+  description = "Paddle Client-side Token"
+  type        = string
+}
+
+variable "paddle_price_ids" {
+  description = "Paddle Price IDs"
+  type        = map(string)
+}
+
+variable "paddle_product_ids" {
+  description = "Paddle Product IDs"
+  type        = map(string)
 }
 
 variable "resource_prefix" {
   description = "Prefix for naming resources. Lower case only, No periods"
+  type        = string
 }
 
 variable "ses_email_address" {
@@ -39,9 +69,9 @@ variable "ses_email_address" {
 }
 
 variable "tags" {
+  default     = {}
   description = "(Optional) A mapping of tags to assign to the resources"
   type        = map(string)
-  default     = {}
 }
 
 variable "tenant_regions" {
