9362ff6ccd4b1fba0fccb45981182cfecf11d86c
Joseph Wortmann <joseph.wortmann@gmail.com>
2024-07-20 23:08:15 +0000

    chore: Remove trailing whitespace in lambda-appsync.tf

--- lambda-appsync.tf
+++ lambda-appsync.tf
@@ -2,7 +2,7 @@
 ##  appsync-datasource  ##
 ###########################
 module "appsync_datasource" {
-  description           = "The main datasource for the echo-stream API "
+  description           = "The main datasource for the echo-stream API"
   dead_letter_arn       = local.lambda_dead_letter_arn
   environment_variables = local.common_lambda_environment_variables
   handler               = "function.handler"
