e8f685e6919a593dfab74159112790e67c5428c1
Joseph Wortmann <joseph.wortmann@gmail.com>
2025-01-26 16:13:01 -0600

    Added a 403 redirect back to index.html

--- cloudfront.tf
+++ cloudfront.tf
@@ -23,6 +23,13 @@ resource "aws_cloudfront_distribution" "webapp" {
 
   custom_error_response {
     error_caching_min_ttl = 0
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
     error_code            = 404
     response_code         = 200
     response_page_path    = "/index.html"
